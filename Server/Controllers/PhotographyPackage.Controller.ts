8fc91f845765f459e197b0cf53c8b8a1f5bbe63d
sarihaiman <sari9741497@gmail.com>
2024-05-23 18:04:42 +0300

    running a eslint on PhotographyPackage

--- Server/Controllers/PhotographyPackage.Controller.ts
+++ Server/Controllers/PhotographyPackage.Controller.ts
@@ -1,12 +1,8 @@
-import fs from 'fs';
 import { Response, Request } from 'express';
-import PhotographyPackage_Model from '../Models/PhotographyPackage.Model';
 import bodyParser from 'body-parser';
-// import {getall} from '../Services/PhotographyPackage.service'
 import { getallPhotographyPackage, addPhotographyPackage, updatePhotographyPackage, deletePhotographyPackage } from '../Services/PhotographyPackage.service';
-
-const fsPromises = require('fs').promises;
-const app = require('express').Router();
+import express from 'express';
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
--- Server/Services/PhotographyPackage.service.ts
+++ Server/Services/PhotographyPackage.service.ts
@@ -1,13 +1,5 @@
-// import fs from 'fs';
 import { Response, Request } from 'express';
 import PhotographyPackage_Model from '../Models/PhotographyPackage.Model';
-// import bodyParser from 'body-parser';
-
-// const fsPromises = require('fs').promises;
-// const app = require('express').Router();
-
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }))
 
 export const getallPhotographyPackage = async function (req: Request, res: Response) {
     const PhotographyPackage = await PhotographyPackage_Model.find();
@@ -16,18 +8,18 @@ export const getallPhotographyPackage = async function (req: Request, res: Respo
 
 export const addPhotographyPackage = async function (req: Request, res: Response) {
     try {
-        let PhotographyPackage = JSON.parse(JSON.stringify(req.body));
+        const PhotographyPackage = JSON.parse(JSON.stringify(req.body));
         const countPhotographyPackage = await PhotographyPackage_Model.find();
         const newPhotographyPackage = {
             "Id": Number(PhotographyPackage.Id),
             "Type": Number(PhotographyPackage.Type),
             "MoneyToHour": Number(PhotographyPackage.MoneyToHour),
         }
-        let length = countPhotographyPackage.length
+        const length = countPhotographyPackage.length
         if (length === 0)
             newPhotographyPackage.Id = 0;
         else {
-            let lengthNow = Number(countPhotographyPackage[length - 1].Id) + 1
+            const lengthNow = Number(countPhotographyPackage[length - 1].Id) + 1
             newPhotographyPackage.Id = lengthNow
         }
         await PhotographyPackage_Model.insertMany(newPhotographyPackage);
@@ -39,8 +31,8 @@ export const addPhotographyPackage = async function (req: Request, res: Response
 
 export const updatePhotographyPackage = async function (req: Request, res: Response) {
     try {
-        let id = Number(req.body.Id);
-        let data = req.body;
+        const id = Number(req.body.Id);
+        const data = req.body;
         if (await PhotographyPackage_Model.findOne({ "Id": id }) === null) {
             res.status(404).send('PhotographyPackage not found');
             return
