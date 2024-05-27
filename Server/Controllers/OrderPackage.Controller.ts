c316ed76fcaceb830abf418012a95a9611fa876e
sarihaiman <sari9741497@gmail.com>
2024-05-27 20:38:25 +0300

    create orderPackage controller

--- Server/Controllers/OrderPackage.Controller.ts
+++ Server/Controllers/OrderPackage.Controller.ts
@@ -1,32 +1,24 @@
 import { Response, Request } from 'express';
 import bodyParser from 'body-parser';
-import { addOrderPackage } from '../Services/OrderPackage.service';
+import { addOrderPackage, getallOrderPackage, updateOrderPackage, deleteOrderPackage } from '../Services/OrderPackage.service';
 import express from 'express';
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-async function requireOrderPackage() {
-
-    // app.get('/PhotographyPackage/getAll', async (req: Request, res: Response) => {
-    //     await getallPhotographyPackage(req, res)
-    // });
-
-    app.post('/OrderPackage/add', async (req: Request, res: Response) => {
-        await addOrderPackage(req, res)
-    })
-
-    // app.put('/PhotographyPackage/update', async (req: Request, res: Response) => {
-    //     await updatePhotographyPackage(req, res);
-    // })
-
-    // app.delete('/PhotographyPackage/delete/:Id', async (req: Request, res: Response) => {
-    //     await deletePhotographyPackage(req, res)
-    // })
-
+export const get = async (req: Request, res: Response) => {
+    await getallOrderPackage(req, res)
 }
 
-requireOrderPackage()
+export const post = async (req: Request, res: Response) => {
+    await addOrderPackage(req, res)
+}
 
-export default app;
+export const put = async (req: Request, res: Response) => {
+    await updateOrderPackage(req, res);
+}
+
+export const deleteOne = async (req: Request, res: Response) => {
+    await deleteOrderPackage(req, res)
+}
