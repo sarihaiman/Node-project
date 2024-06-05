936755d9599953e973e1773c558d9662f2fa5a3a
sarihaiman <sari9741497@gmail.com>
2024-06-05 12:35:35 +0300

    create user router

---
+++ Server/Routers/user.router.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { get, post, put, deleteOne } from '../Controllers/user.controller';
+const router = express.Router();
+
+router.get('/user', get)
+router.post('/user', post)
+router.put('/user/:id', put)
+router.delete('/user/:id', deleteOne)
+
+export default router
