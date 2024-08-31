4ab4199121b71fee68261b162e2d99dcb7bd2ec1
sarihaiman <sari9741497@gmail.com>
2024-09-01 01:43:51 +0300

    add swagger to Business

--- Server/Routers/business.router.ts
+++ Server/Routers/business.router.ts
@@ -4,7 +4,54 @@ import aouthentication_admin from '../Middleware/aouthentication_admin.middlewar
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /business:
+ *   get:
+ *     summary: Get all business
+ *     tags: [business]
+ *     description: An example endpoint that returns example data
+ *     responses:
+ *       200:
+ *         description: successful operation
+ */
 router.get('/business', get)
-router.put('/business',aouthentication_admin, put)
+
+
+/**
+ * @swagger
+ * /business:
+ *   put:
+ *     summary: Update an business
+ *     tags: [business]
+ *     requestBody:     
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               adress:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - adress
+ *               - phone
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *       400:
+ *         description: Invalid ID supplied
+ *       404:
+ *         description: business not found
+ */
+router.put('/business', put)
+
+// router.put('/business',aouthentication_admin, put)
 
 export default router;
