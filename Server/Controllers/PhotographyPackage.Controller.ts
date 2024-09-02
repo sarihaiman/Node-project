19401eab22767be8a9a54c0516788c0256483a5d
sarihaiman <sari9741497@gmail.com>
2024-09-02 14:53:43 +0300

    add log to PhotographyPackage

--- Server/Controllers/PhotographyPackage.Controller.ts
+++ Server/Controllers/PhotographyPackage.Controller.ts
@@ -2,24 +2,29 @@ import { Response, Request } from 'express';
 import bodyParser from 'body-parser';
 import { getallPhotographyPackage, addPhotographyPackage, updatePhotographyPackage, deletePhotographyPackage } from '../Services/PhotographyPackage.service';
 import express from 'express';
-const app = express()
+import logger from '../logger'; // Import the logger
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 export const get = async (req: Request, res: Response) => {
-    await getallPhotographyPackage(req, res)
+    logger.info(`GET request received at ${req.path}`);
+    await getallPhotographyPackage(req, res);
 };
 
 export const post = async (req: Request, res: Response) => {
-    await addPhotographyPackage(req, res)
-}
+    logger.info(`POST request received at ${req.path}`);
+    await addPhotographyPackage(req, res);
+};
 
 export const put = async (req: Request, res: Response) => {
+    logger.info(`PUT request received at ${req.path}`);
     await updatePhotographyPackage(req, res);
-}
+};
 
 export const deleteOne = async (req: Request, res: Response) => {
-    await deletePhotographyPackage(req, res)
-}
-
+    logger.info(`DELETE request received at ${req.path}`);
+    await deletePhotographyPackage(req, res);
+};
