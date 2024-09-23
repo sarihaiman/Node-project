09752a4de0182d7c866d0dd88c811bf3422952f1
sarihaiman <sari9741497@gmail.com>
2024-09-23 12:12:54 +0300

    Sending a token in addOrderPackage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './them.ts'; 
 import App from './App.tsx';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+// import 'primeicons/primeicons.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
